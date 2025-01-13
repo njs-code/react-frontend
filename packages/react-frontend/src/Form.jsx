21508959f9cc49e4a807aa748e5c5cd5472354c3
Noah Snodgrass <419noah@protonmail.com>
2025-01-13 13:11:06 -0800

    Introduced form input functionality

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,17 +1,29 @@
 import React, {useState} from 'react';
 
-function Form(){
+//A function component to handle the input form
+function Form(props){
+    //defines the characters in the table
     const[person, setPerson] = useState({
         name: "",
         job: ""
     });
 
+    //inner function to remove from list of characters 
     function handleChange(event){
         const{name,value} = event.target;
         if (name === "job") 
-            setPerson({name: person["name"], job: value});
+            setPerson({ name: person["name"], 
+                        job: value});
         else 
-            setPerson({name: value, job:person["job"]});
+            setPerson({
+                        name: value, 
+                        job:person["job"]});
+    }
+
+    //inner function to update list of characters
+    function submitForm(){
+        props.handleSubmit(person);
+        setPerson({name:"", job:""});
     }
 
     return (
@@ -32,6 +44,10 @@ function Form(){
             value={person.job}
             onChange={handleChange}
           />
+          <input 
+            type="button" 
+            value="Submit" 
+            onClick={submitForm} />
         </form>
       );
 }
