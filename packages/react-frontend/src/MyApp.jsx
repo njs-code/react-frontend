2eb73a42eae767015bea42a5f0a7516c4ce3a9c3
Noah Snodgrass <419noah@protonmail.com>
2025-01-13 13:15:35 -0800

    Comments

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Table from './Table';
 import Form from "./Form";
-
-//function component  
+ 
 function MyApp(){
+    //defines characters list as state
     const [characters, setCharacters] = useState([]);
 
+    //function to filter/remove a character from the state
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
           return i !== index;
@@ -13,10 +14,14 @@ function MyApp(){
         setCharacters(updated);
       };
 
+    //function to add a character to the state
     function updateList(person){
         setCharacters([...characters, person]);
     }
 
+    //html for webpage: 
+        //table for character display (props pass characters and enable removal)
+        //form for new character submission  
     return (
       <div className='container'>
         <Table 
