11daf847c35c718d4f712ddcb75dc0a3d9174170
Noah Snodgrass <419noah@protonmail.com>
2025-01-12 19:47:45 -0800

    Remove button functionality

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -4,6 +4,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -12,8 +13,13 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
+            <td>{row.name}</td>
+            <td>{row.job}</td>
+            <td>
+                <button onClick={() => props.removeCharacter(index)}>
+                Delete
+                </button>
+            </td>
         </tr>
       );
      }
@@ -29,7 +35,9 @@ function TableHeader() {
       return (
         <table>
           <TableHeader />
-          <TableBody characterData={props.characterData} />
+          <TableBody characterData={props.characterData} 
+          removeCharacter={props.removeCharacter}
+          />
         </table>
       );
   }
